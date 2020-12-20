8be0209ebb91d3dab7dca515917b10d18dcf5633
atul219 <atulyadav219@gmail.com>
2020-12-20 18:31:12 +0530

    Enemy AI Code added (Doesn't work atm)
    Code added as comments in the enemyAI file however it isn't working with the characters and 3d objects atm

--- Assets/Enemy/enemyAI.cs
+++ Assets/Enemy/enemyAI.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿/**
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -82,3 +84,99 @@ public class enemyAI : MonoBehaviour
 
     }
 }
+
+**/
+
+//using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField] Transform player;
+    [SerializeField] float chaseRange = 10f;
+    [SerializeField] float turnSpeed = 5f;
+
+    NavMeshAgent navMeshAgent;
+
+    float distanceToPlayer = Mathf.Infinity;
+
+    bool isProvked = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        distanceToPlayer = Vector3.Distance(player.position, transform.position);
+
+        if (isProvked)
+        {
+            EngagePlayer();
+        }
+
+        else if (distanceToPlayer <= chaseRange)
+        {
+            // if player is visible
+            isProvked = true;
+        }
+
+
+    }
+
+    // engage player if its within chase range and attack if its within stopping distance
+    private void EngagePlayer()
+    {
+        FacePlayer();
+
+        if (distanceToPlayer >= navMeshAgent.stoppingDistance)
+        {
+            ChasePlayer();
+        }
+
+        if (distanceToPlayer <= navMeshAgent.stoppingDistance)
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void ChasePlayer()
+    {
+
+      //  GetComponent<Animator>().SetBool("attack", false);
+      //  GetComponent<Animator>().SetTrigger("move");
+        navMeshAgent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+       // GetComponent<Animator>().SetBool("attack", true);
+    }
+
+    // to move enemy face 
+    private void FacePlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
+
+
+    // display the chase range
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+
+
+}
+
