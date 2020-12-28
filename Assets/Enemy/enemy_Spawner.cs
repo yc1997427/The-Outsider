bc8fb458bf73dc732b8303b9c1290465b8e27341
salmanriazsyed <72282430+salmanriazsyed@users.noreply.github.com>
2020-12-28 01:17:08 +0000

    spawner implemented but position broken
    spawner has spawn points as children however the position of the spawned enemy is wrong (it is the position of spawn point + position of parent spawner). enemy is spawned at the press of "t" (for testing)

---
+++ Assets/Enemy/enemy_Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_Spawner : MonoBehaviour
+{
+    public GameObject[] spawnpoints;
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        spawnpoints = new GameObject[5];
+        
+        for(int i=0; i<spawnpoints.Length; i++)
+        {
+            spawnpoints[i] = transform.GetChild(i).gameObject;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            int id = Random.Range(0, spawnpoints.Length);
+            Instantiate(enemy, spawnpoints[id].transform.position, spawnpoints[id].transform.rotation);
+        }
+    }
+}
