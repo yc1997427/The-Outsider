e9fb87fff794c22fe95544ba38c76137186a3090
Atul Yadav <33904670+atul219@users.noreply.github.com>
2020-12-31 08:06:47 +0530

    Update playerhealth.cs
    update player health

--- Assets/Player/playerhealth.cs
+++ Assets/Player/playerhealth.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 
 public class playerhealth : MonoBehaviour
 {
-    // Start is called before the first frame update
+  
+  /**
+  // Start is called before the first frame update
     public float curhealth;
     public float maxhealth;
 
@@ -29,14 +31,14 @@ public class playerhealth : MonoBehaviour
        
         	anim.SetFloat("hit",hit);
         }
-        /*if(curhealth<1){
+        if(curhealth<1){
             GetComponent<CapsuleCollider>().direction=2;
         	anim.SetBool("death",true);
 
             
       
             //collider.direction=2;
-        }*/
+        }
         if(Input.GetKeyUp(KeyCode.Return)){
         	SendDamage(Random.Range(10,20));
         }
@@ -60,5 +62,66 @@ public class playerhealth : MonoBehaviour
     	healthBar.value=curhealth;
         Debug.Log(curhealth);
     	anim.SetFloat("hit",1);
+    } 
+    
+    */
+    public static playerhealth singleton;
+    public float currentHealth;
+    public float maxHealth = 100f;
+    public bool isDead = false;
+
+
+    public Slider healthBar;
+    public Text healthcounter;
+    public GameObject player;
+    private Animator anim;
+
+
+    void Start()
+    {
+        singleton = this;
+        currentHealth = maxHealth;
+        healthBar.value = currentHealth;
+        healthBar.maxValue = maxHealth;
+        anim = GetComponent<Animator>();
+        UpdateHealthCounter();
+    }
+
+
+    public void DamagePlayer(float damage)
+    {
+
+        if (currentHealth > 0)
+        {
+            if (damage >= currentHealth)
+            {
+                Dead();
+
+            }
+            else
+            {
+                currentHealth -= damage;
+                // healthBar.value = currentHealth;
+                healthBar.value -= damage;
+            }
+
+            UpdateHealthCounter();
+        }
+
+
+    }
+
+    void Dead()
+    {
+        currentHealth = 0;
+        isDead = true;
+        healthBar.value = 0;
+        UpdateHealthCounter();
+        Debug.Log("Player is Dead");
+    }
+
+    void UpdateHealthCounter()
+    {
+        healthcounter.text = currentHealth.ToString();
     }
 }
