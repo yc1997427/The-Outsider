55add88f8e23da6a67806ffbdff9223ce7a66525
salmanriazsyed <72282430+salmanriazsyed@users.noreply.github.com>
2021-01-03 18:49:54 +0000

    Castle and Ending

---
+++ Assets/BurryAshes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurryAshes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Press E to burry ashes.");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("THE END!");
+        }
+    }
+}
