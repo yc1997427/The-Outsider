e0c4b9d5f754da9af819d2798995fcd1c6e1032f
salmanriazsyed <72282430+salmanriazsyed@users.noreply.github.com>
2021-01-05 22:46:47 +0000

    Level design and invisible boxes (to show progress)

---
+++ Assets/Player/BurryAshes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurryAshes : MonoBehaviour
+{
+    public Canvas CanvasObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Press E to burry ashes.");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            CanvasObject.GetComponent<Canvas>().enabled = false;
+            Debug.Log("THE END!");
+        }
+    }
+}
