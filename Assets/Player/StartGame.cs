167458902184e6544b957f10001f170b66a08ed4
yc997 <1084155272@qq.com>
2021-01-07 04:43:08 +0000

    Revert "revert"
    This reverts commit f26c6c3bdc2fb4490a61c4dc2ce31b737ff279c7.

---
+++ Assets/Player/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void ReloadGame()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Player/playerhealth.cs
+++ Assets/Player/playerhealth.cs
@@ -96,6 +96,7 @@ public class playerhealth : MonoBehaviour
         {
             Dead();
             healthcounter.text = curhealth.ToString();
+            GetComponent<DeathHandler>().HandleDeath();
 
         }
 
